f4cfbd39ec4cef34471fbc22c8b40699520ce437
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-05-20 12:08:03 -0400

    in main.js add SHOW SCROLL TOP section, create scrollTop function and add event listener to window to call function on scroll; in style.css add Show scrolltop section, add styling to scroll-top

--- js/main.js
+++ js/main.js
@@ -50,3 +50,10 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader)
 
 // SHOW SCROLL TOP
+function scrollTop(){
+  const scrollTop = document.getElementById('scroll-top')
+
+  if(this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top')
+}
+
+window.addEventListener('scroll', scrollTop)
